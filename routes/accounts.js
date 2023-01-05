de8275c2cb224f7b19d49c6f7709413edaf55706
DJehSantana <jessicah.dias23@hotmail.com>
2023-01-05 12:35:48 -0300

    Criado método post na rota account

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,5 +1,38 @@
 import express from 'express';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
+
+const jsonDb = 'accounts.json';
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+
+    try {
+        //pega os parâmetros do body
+        let newAccount = req.body;
+        const database = JSON.parse(await readFile(jsonDb));
+        console.log(database);
+
+        if (newAccount.name && newAccount.balance) {
+            //adciona o id indicado no arquivo json a propriedade id da nova conta cadastrada
+            newAccount = {
+                id: database.nextId++,
+                ...newAccount
+            }
+            //adiciona novo cadastro ao BD
+            database.accounts.push(newAccount);
+            // 2 - adiciona espaço na hora de salvar as informações, melhora a formatação do documento
+            await writeFile(jsonDb, JSON.stringify(database, null, 2));
+        } else {
+            throw new Error('Invalid fields');
+        }
+        res.send(newAccount);
+
+    } catch (e) {
+        res.status(400).send({ error: e.message });
+    }
+});
 
 export default router;
